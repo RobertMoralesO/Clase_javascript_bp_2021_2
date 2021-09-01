d80f8eb6ca7c9b0cf43f73d25f9dfbde8a6eaacc
Roberto Morales <roberto@morales.com.co>
2021-08-31 19:42:25 -0500

    Hola mundo, variables y constantes

---
+++ script.js
@@ -0,0 +1,54 @@
+// Hola Mundo
+//alert('Hello world');
+
+// Comentarios
+
+// Comentario de línea
+/*
+Comentario de mas 
+de una
+línea
+*/
+
+// Variables
+let mensaje;
+
+mensaje = "Hola Mundo";
+//alert(mensaje);
+
+mensaje = "Hola Mundo dos";
+//alert(mensaje);
+
+mensaje = "Hola Mundo tres";
+//alert(mensaje);
+
+let a= 3, b= 4, c = 5;
+console.log(a + b);
+
+let nombre = "Roberto";
+let apellido = "Morales";
+
+console.log("Hola",nombre,apellido);
+
+// Nombrar Variables
+
+// Camel Case
+let nombreUno = "Roberto";
+let nombreDos = "Morales";
+
+// Nombrar como underscore
+nombre_uno = "Roberto";
+nombre_dos = "Morales";
+
+// Nombre no comunes
+let $ = 1;
+let _ = 2;
+console.log($,_);
+
+// No puedo nombrar variables
+// let 1a = 3;
+// let primer-nombre = 4;
+
+// Constantes
+const PI = 3.141624;
+const COLOR_NEGRO = "#FFFFF";
